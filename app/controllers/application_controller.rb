8ab850b204fdb06c96e48fa469bdc2075529f0e0
Jucavi <jcarvil@gmail.com>
2021-01-24 03:39:43 +0100

    Configure Rails Admin integration with CanCan adn Devise Handle CanCan Access Denied Exeption

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    recipes_path
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_path, alert: exception.message
+  end
 end

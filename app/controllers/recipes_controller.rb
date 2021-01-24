25fe1bfb2d84d40185963a4ddc10a1297516c2f4
Jucavi <jcarvil@gmail.com>
2021-01-24 19:15:44 +0100

    Add Devise messages with toastr

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,5 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
-  # load_and_authorize_resource
   skip_before_action :authenticate_user!, :only => [:index]
 
   def index

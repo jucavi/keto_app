6f08ca2c6398ae10f0c8fc53772949d153e2c05f
Jucavi <jcarvil@gmail.com>
2021-03-16 21:07:22 +0100

    Refactor Navbar

--- app/javascript/packs/ingredient.js
+++ app/javascript/packs/ingredient.js
@@ -41,7 +41,6 @@ $(document).on('ready page:load turbolinks:load', function() {
 
   $("#recipe_ingredients")
   .on('cocoon:after-insert', function(e, addedItem) {
-    console.log('after insert');
     $(addedItem).find('.selectize').selectize({
       create: function(input, callback) {
         selectizeCallback = callback;

4b6fdf2efa774a5145717cb2ef730b9bbe223ce4
Jucavi <jcarvil@gmail.com>
2021-03-16 18:55:52 +0100

    Set Start Bootstrap Grayscale theme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,24 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import JQuery from 'jquery';
+window.$ = window.jQuery = JQuery;
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Fontawesome
+import "@fortawesome/fontawesome-free/css/all"
+
 // Bootstrap
 require("bootstrap")
+
+// Start Bootstrap Grayscale Theme
+require("jquery.easing/jquery.easing")
+require("packs/scripts")
+const images = require.context('../images', true)
 
 // Toastr
 global.toastr = require("toastr")
@@ -20,6 +30,7 @@ require("@nathanvda/cocoon")
 // Selectize
 require("selectize")
 require("packs/ingredient")
+
 
 import "../stylesheets/application"
 
